172e57a5c01e9f5cdc58fd7db9d76b958b586276
Niv <nivb52@gmail.com>
2021-08-12 09:53:23 +0300

    change stracture and config for typescript

--- admin/src/server/index.ts
+++ admin/src/server/index.ts
@@ -3,6 +3,10 @@ const PORT = 4000;
 
 import { Request, Response, Application } from 'express';
 import express = require('express');
+// import  db from './db';
+import { PrismaClient } from '@prisma/client'
+
+const db = new PrismaClient();
 
 const app: Application = express();
 
@@ -11,11 +15,19 @@ app.use(cors({
     origin: ['http://localhost:3000', 'http://localhost:8080']
 }));
 
+async function main(){
+    return await db.user.findMany()
+}
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World')
 });
 
+
+app.get('/u', async function (req: Request, res: Response) {
+    const users = await main();
+    res.send(users)
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
