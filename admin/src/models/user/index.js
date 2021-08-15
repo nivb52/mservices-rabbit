01106e2f0ab8b8cde4faf49ae98a7d96c62c7314
Niv <nivb52@gmail.com>
2021-08-15 16:44:13 +0300

    crud routes with related db operation

--- admin/src/models/user/index.js
+++ admin/src/models/user/index.js
@@ -1,12 +1,116 @@
 import dbConnection from '../../server/db'
 
-export const create = async function(user_data) {
+export const create = async function(userData) {
     const db = await dbConnection;
     return db.user.create({
         data: {
-            firstName: user_data.firstName,
-            lastName: user_data.lastName,
-            email: user_data.email,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            email: userData.email,
         }
     });
 }
+
+export const updateById = async function (id, newData) {
+    if (newData.email) {
+        throw new Error('this field cannot be edited')
+    }
+    const parsedId = Number.parseInt(id)
+    if (!parsedId) throw new Error('not valid')
+    const db = await dbConnection;
+    return db.user.update({
+        where: {
+            id: parsedId,
+        },
+        data: {
+            firstName: newData.firstName,
+            lastName: newData.lastName,
+        }
+    });
+}
+
+
+export const deleteById = async function (id) {
+    const parsedId = Number.parseInt(id)
+    if (!parsedId) throw new Error('not valid')
+    const db = await dbConnection;
+    return db.user.delete({
+        where: {
+            id: parsedId,
+        },
+    });
+}
+
+export const deleteByEmail = async function (email) {
+    const db = await dbConnection;
+    return db.user.delete({
+        where: {
+            email: email,
+        },
+    });
+}
+
+export const findById = async function (id) {
+    const parsedId = Number.parseInt(id)
+    if (!parsedId) throw new Error('not valid')
+    const db = await dbConnection;
+    return db.user.findUnique({
+        where: {
+            id: parsedId,
+        },
+    });
+}
+
+export const findOne = async function ({ where, orderBy = {} },  isUnique = true) {
+    const db = await dbConnection;
+    const searchData = { where: { where }, orderBy: { ...orderBy, id: 'asc' } }
+     if (isUnique) {
+        return db.user.findUnique({
+            searchData,
+        });
+      } else {
+        return db.user.findFirst({
+            searchData,
+        });
+    }
+};
+export const findMany = async function ({ where, orderBy = {} }, limit = 10) {
+    const db = await dbConnection;
+    const searchData = { where: { where }, orderBy: { ...orderBy, id: 'asc' } , limit: { limit }}
+        return db.user.findMany({
+            searchData,
+        });
+};
+ export const update = async function (where, newData) {
+     if (newData.email) {
+         throw new Error('this field cannot be edited')
+     }
+     const db = await dbConnection;
+     return db.user.update({
+         where: {
+             where,
+         },
+         data: {
+             firstName: newData.firstName,
+             lastName: newData.lastName,
+         }
+     });
+ }
+
+ export const upsert = async function (where, newData) {
+     const db = await dbConnection;
+     return db.user.upsert({
+         where: {
+             where,
+         },
+         update: {
+             firstName: newData.firstName,
+             lastName: newData.lastName,
+         },
+         create: {
+             firstName: newData.firstName,
+             lastName: newData.lastName,
+             email: newData.email,
+         }
+     });
+ };
--- admin/src/server/index.ts
+++ admin/src/server/index.ts
@@ -22,10 +22,26 @@ app.get('/users', async function (req: Request, res: Response) {
     res.send(JSON.stringify(users))
 });
 
+
+app.get('/users/:id', async (req: Request, res: Response) => {
+    const user = await User.findById(req.params.id)
+    return res.send(user)
+})
+
 app.post('/users', async (req: Request, res: Response) => {
     const new_user = await User.create(req.body);
     return res.send(JSON.stringify(new_user))
 });
+
+app.put('/users/:id', async (req: Request, res: Response) => {
+    const updated_user = await User.updateById(req.params.id, req.body);
+    return res.send(JSON.stringify(updated_user))
+});
+
+app.delete('/users/:id', async (req: Request, res: Response) => {
+    const result = await User.deleteById(req.params.id);
+    return res.send(result)
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
