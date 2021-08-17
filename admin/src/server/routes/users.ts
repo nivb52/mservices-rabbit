d084d28483c970c2a0377d298498bd5b056ee3c0
Niv <nivb52@gmail.com>
2021-08-17 04:15:37 +0300

    add rabbitmq

--- admin/src/server/routes/users.ts
+++ admin/src/server/routes/users.ts
@@ -4,15 +4,27 @@ const router: Router = Router();
 import dbConnection from '../db';
 import * as User from '../../models//user/index';
 
+import PubSub from '../pubsub/';
+
+
+const q = 'users'; //
+const publisher = new PubSub('Users', 'users', q);
+const consumer = new PubSub('Users', 'users', q);
+// await consumer.init();
+const txt = 'something to do'
+// Publisher
+
 
 router.get('/', async function (req: Request, res: Response) {
-    const db = await dbConnection;
-    const users = await db.user.findMany()
+    const db = dbConnection;
+    const users = await db.user.findMany();
+    await publisher.init();
+    publisher.publish(txt);
     res.send(JSON.stringify(users))
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
-    const user = await User.findById(<String | Number> req.params.id)
+    const user = await User.findById(<string | number> req.params.id)
     return res.send(user)
 })
 
@@ -22,13 +34,13 @@ router.post('', async (req: Request, res: Response) => {
 });
 
 router.put('/:id', async (req: Request, res: Response) => {
-    const updated_user = await User.updateById(<String | Number> req.params.id, req.body);
+    const updated_user = await User.updateById(<string | number> req.params.id, req.body);
     return res.send(JSON.stringify(updated_user))
 });
 
 router.delete('/:id', async (req: Request, res: Response) => {
-    const result = await User.deleteById(<String | Number> req.params.id);
+    const result = await User.deleteById(<string | number> req.params.id);
     return res.send(result)
 })
 
-export default ;
+export default router;
